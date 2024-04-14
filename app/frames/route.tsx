9b1656e0050c1281b2fde49e4d4d374ff15010a8
Nicholas McGuire <nickmcguire.zero@gmail.com>
2024-04-14 15:09:19 -0700

    Add some frame logic

--- app/frames/route.tsx
+++ app/frames/route.tsx
@@ -7,6 +7,29 @@ export const GET = frames(async (ctx) => {
   const currentUrl = new URL(ctx.url.toString());
   currentUrl.pathname = '/frames';
 
+  const {inputText} = ctx.message || {};
+
+  if (inputText) {
+    // check valid readwise key
+    const isValidKey = await isValidReadwiseKey(inputText);
+
+    if (isValidKey) {
+      // jwt encode, store in postgres, return frame to install action
+    } else {
+      return {
+        image: <div>Invalid Readwise key, double check your key</div>,
+        textInput: 'Input Readwise key',
+        buttons: [<Button action="post">Submit</Button>],
+      };
+    }
+  } else {
+    return {
+      image: <div>Save to Readwise: input readwise key to get started</div>,
+      textInput: 'Input Readwise key',
+      buttons: [<Button action="post">Submit</Button>],
+    };
+  }
+
   const installActionUrl = constructCastActionUrl({
     actionType: 'post',
     icon: 'number',
@@ -32,3 +55,22 @@ export const POST = frames(async (ctx) => {
     message: `The user's FID is ${ctx.message?.castId?.fid}`,
   });
 });
+
+const isValidReadwiseKey = async (key: string) => {
+  try {
+    const response = await fetch('https://readwise.io/api/v2/auth/', {
+      headers: {
+        Authorization: `Token ${key}`,
+      },
+    });
+
+    if (response.status === 200) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (err) {
+    console.log(`Error checking Readwise key: ${err}`);
+    return false;
+  }
+};
